fd0e29ecc9d23fc24cd286b56d6ac3cf208a7c02
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-02-07 10:51:03 -0500

    added api routes and deleted some comments out of other files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,19 @@
-// Your apiRoutes.js file should contain two routes:
+// LOAD DATA
 
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+const friends = require("../data/friends");
+
+// ROUTING
+
+module.exports = function(app) {
+    // API GET requests
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    })
+    
+    // API POST requests
+    app.post("api/friends", function(req, res) {
+        // BUILD LOGIC HERE
+        // THINGS TO DO HERE
+        // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+    });
+};
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// Your server.js file should require the basic npm packages we've used in class: express and path.
-
 var express = require("express");
 var app = express();
 
