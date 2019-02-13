2451f8757876deb0e000419608a434008de78cfc
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-02-13 13:32:05 -0500

    added more potential friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,86 @@ const friendsArray = [
             3,
             1
         ]
+    },
+    {
+        name: "Darth Vader",
+        photo: "https://starwarsblog.starwars.com/wp-content/uploads/2017/03/darth-vader-1-cover-header-1536x864.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Michael Scott",
+        photo: "https://imgix.bustle.com/uploads/image/2018/4/9/0e1587de-fd74-4e77-9264-62a0bf5e894a-nup_130296_0067.jpeg?w=1020&h=574&fit=crop&crop=faces&auto=format&q=70",
+        scores: [
+            3,
+            2,
+            3,
+            5,
+            4,
+            3,
+            2,
+            2,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Leonardo DiCaprio",
+        photo: "http://specials-images.forbesimg.com/imageserve/558c0172e4b0425fd034f8ba/280x425.jpg?fit=scale&background=000000",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Michelle Obama",
+        photo: "https://www.gannett-cdn.com/presto/2018/12/11/PMJS/6d6048e3-56f9-46e4-b016-421d961dd0f7-MichelleObama_8x10.jpg?width=534&height=712&fit=bounds&auto=webp",
+        scores: [
+            5,
+            4,
+            5,
+            4,
+            5,
+            4,
+            4,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Arthur",
+        photo: "https://christmasspecialreviews.files.wordpress.com/2011/12/arthur-pbs-kids.jpeg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            2,
+            3,
+            1
+        ]
     }
 ];
 
